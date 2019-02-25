c730166f05a26b48eec63d111973f4a8325879a7
Shelisha Rutherford <starshipazul@yahoo.com>
2019-02-24 23:11:32 -0500

    HW5

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TriviaGame
 HW5
+This is a trivia form with multiple choice or True/false options are used.
+You only have a limited amount of time to finish the quiz.
+This Game will end when the time runs out. 
+You will see the number of answers that are correct.
+You are only allowed to pick one answer per question.
---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+
+var time;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Create a trivia form with multiple choice or True/false options (my choice)
+// Player will have a limited amount of time to finish the quiz
+// Game will end when the time runs out. Page
+// Page must reveal the number of questions that the players answer correctly
+// Player can only pick one answer per question
