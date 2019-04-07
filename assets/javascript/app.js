6f720434a84e458a497bf03627b7eeb1206f7e34
Shelisha Rutherford <starshipazul@yahoo.com>
2019-04-07 19:28:36 -0400

    HW5 changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,160 +1,112 @@
-
-var time = 25;
+var timer = 60;
 var timerId;
-var correct = 0;
-var missed = 0;
+var NumQuestions = 5;
+var score = 0;
+var correctA = ["a", "b", "a", "a", "b"];
+var results = 0;
 
 
 
 
-var questions = [
-    {
-        question: "Lt Commander Worfs son named is Alexander?",
-        answers: ["true", "false"],
-        correctAns: "true"
 
-    },
 
-    {
-        question: "Captin Pichard was never a Borg?",
-        answers: ["true", "false"],
-        correctAns: "false"
 
-    },
-    {
-        question: "Commandor Data has a borther named Lor?",
-        answers: ["true", "false"],
-        correctAns: "true"
+
+
+
+
+
+
+
+function yourAnswers() {
+  var q1 = document.forms["trivia"]["q1"].value;
+  var q2 = document.forms["trivia"]["q2"].value;
+  var q3 = document.forms["trivia"]["q3"].value;
+  var q4 = document.forms["trivia"]["q4"].value;
+  var q5 = document.forms["trivia"]["q5"].value;
+
+
+
+  //gives an alert to the user that the messed a question
+  if (q1 === null || q1 === "") {
+    alert("Where is number 1");
+    return false;
+  }
+  if (q2 === null || q2 === "") {
+    alert("Where is number 2");
+    return false;
+  }
+  if (q3 === null || q3 === "") {
+    alert("Where is number 3");
+    return false;
+  }
+  if (q4 === null || q4 === "") {
+    alert("Where is number 4");
+    return false;
+  }
+  if (q5 === null || q5 === "") {
+    alert("Where is number 5");
+    return false;
+  }
+
+
+  //$(document).ready(function () {
+  //function startGame() {
+  // $("#startGame").hide();
+  // $("#endGame").show();
+  //decrementTime();
+
+
+  // this is suppose to work with my timer. My tutor helped me with this one. However i can't get it to work anymore.
+
+  //   for (var i = 0; i < 5; i++) {
+  //      $("#question").append("<h2>" + question[i].question + "</h2>");
+  //      for (var a = 0; a < question[i].correctA.lenght; a++) {
+  //          $("#question").append("<input type= 'radio' name-'question" + i + " 'value='" + question[i].correctA[a] + "'>'" + question[i].answers[a]);
+  //      }
+
+  // }
+
+
+  // function startTime () {
+  //   time = time - 1;
+  //   if (time < 60) {
+  //     startTime.innerHTML = time;
+  //   }
+  //   if (time < 1) {
+  //     window.clearInterval(reset);
+  //   }
+  // }  
+
+  // reset = setInterval("startTime()", 1000);
+
+  // $("#startGame").click(function(){
+  // //   
+  //   counter = setInterval(timer, 60);
+  // //  
+  //  });
+
+  // function timer() {
+  //   count--;
+  //   if (count <= 0) clearInterval(counter);
+  //   return;
+  // }
+
+  // $("startTime").html("Time: " + "00: " + count + "secs"); {
+
+
+
+  //put for loop here
+  for (i = 1; i <= NumQuestions; i++) {
+    if (eval("q" + i) === correctA[i - 1]) {
+      score++;
     }
 
-];
+  }
 
-// setInterval(() => {
+  //displays the score
+  var results = document.getElementById("results");
+  results.innerHTML = "<h3> Your got <span>" + score + " out of " + NumQuestions + "</span></h3>";
 
-//  }, interval);
-//  setTimeout(() => {
-
-//  }, timeout);
-
-function decrementTime() {
-
-    timerId = setInterval(function () {
-        time--;
-        console.log("time")
-        $("#time").html(time);
-
-        if (time === 0) {
-            clearInterval(timerId)
-            endGame();
-        }
-
-    }, 1 * 1000);
+return false;
 }
-$(document).ready(function () {
-    //function startGame() {
-    // $("#startGame").hide();
-    // $("#endGame").show();
-     //decrementTime();
-
-    for (var i = 0; i < questions.length; i++) {
-        $("#questions").append("<h2>" + questions[i].question + "</h2>");
-        for (var a = 0; a < questions[i].answers.lenght; a++) {
-            $("#questions").append("<input type= 'radio' name-'question" + i + " 'value='" + questions[i].answers[a] + "'>'" + questions[i].answers[a]);
-        }
-
-    }
-
-
-});
-
-function endGame() {
-    clearInterval(timerId)
-}
-
-
-$(document).ready(function () {
-
-    $("#startGame").on("click", function () {
-        console.log("start game")
-
-        $("#startGame").hide();
-        decrementTime();
-
-    })
-
-    $("#endGame").on("click", function () {
-        endGame();
-    })
-})
-
-
-
-
-
-
-
-
-
-    //submitButton.addEventListener('click', showResults);
-
-
-
-
-
-
-
-//$("#qOne").on("submit", function(event) {
-    ////event.preventDefault();
-
-//}
-
-//)
-
-//}
-
-
-
-
-
-//function startGame()
-//$(".start-button").on("click");
-
-
-//var qOne = [
-    //{
-     // qOne: "What was Lt. Commandor Worf's sons name?",
-     // answers: {
-      //  a: "Worf Jr.",
-     //   b: "Alexander",
-     //   c: "Andrew",
-    //  },
-     // correctAnswer: "c"
-
-     // function buildQuiz(){}
-
-//function showResults(){}
-
-// display quiz right away
-//buildQuiz();
-
-// on submit, show results
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Create a trivia form with multiple choice or True/false options (my choice)
-// Player will have a limited amount of time to finish the quiz
-// Game will end when the time runs out. Page
-// Page must reveal the number of questions that the players answer correctly
-// Player can only pick one answer per question.
---
+++ assets/javascript/app2.js
@@ -0,0 +1,121 @@
+// Process flow for this javascript
+//create variables to use. Totale Questions, score, correct answers, results
+//create a function to capture the users selection for each question
+//create a prompt that will warn the user when they have missed a quesiton
+//validate the users response to the correct answer
+// display score to user
+// create a display with the correct answers for the users
+//create a timer for the game
+//wanted to define the onclick functions that users will use to start and stop the game
+window.onload = function () {
+ $("#startButton").on("click", timer.start);
+ $("#done").on("click", timer.stop);
+}
+//these are the variables that I created for this js
+var totalquestions = 5;
+var score = 0;
+var correctAnswer = ["a", "b", "a", "a", "b"];
+var results = 0;
+var runningClock = false;
+var timer = 60;
+
+//function that will capture the users response for each question. I used document.getElentByID first since that is what I have been using but could not get it to work. I found document.forms on https://www.w3schools.com/jsref/coll_doc_forms.asp and this worked.
+//this will store the users' answers in memeory but will not display-that will come later.
+function userAnswers() {
+ var q1 = document.forms["triviaGame"]["q1"].value;
+ var q2 = document.forms["triviaGame"]["q2"].value;
+ var q3 = document.forms["triviaGame"]["q3"].value;
+ var q4 = document.forms["triviaGame"]["q4"].value;
+ var q5 = document.forms["triviaGame"]["q5"].value;
+ console.log(q1, q2, q3, q4, q5);
+}
+
+
+
+//starting the timer
+// startTimer: function() {
+//  $("#timerCount").text("Time Remaing: " + timer);
+//  setInterval(timerCount, 1000);
+// }
+
+
+// decrement timer and stop at 0
+// countDown: function () {
+//  timer--;
+//  $("#timerCount").text("Time Remaining: " + timer);
+//  if (timer === 0) {
+//   stopTimer();
+//   $("#timer").empty();
+//  }
+// }
+// ​
+//created this alert to make sure the for is recongizing the users input.
+//alert(q1);
+//adding in a check to make sure users can't click submit without filling out the full form. I am only checking one question before creating the for loop.
+// if(q1 === null || q1 === "") {
+// alert("You didn't answer question 1");
+//return false;
+// }
+
+//This is the for loop that I tried to use.  It is not working as I wanted.  Will come back later and try to make it work.
+//for(i=1; i <= totalQuestions; i++) {
+//if(eval("q"+i) === null || eval("q"+i) === "") {
+//alert("You didn't answer question " + i);
+//return false;    
+//}
+
+// //Since I know the "if" alert worked from test earlier I am going to use that same format for each question. I understand this is not "DRY" and will work on the for loop later.
+// if (q1 === null || q1 === "") {
+//  alert("You didn't answer question 1");
+// // return false;
+// };
+// if (q2 === null || q2 === "") {
+//  alert("You didn't answer question 2");
+//  //return false;
+// }
+// if (q3 === null || q3 === "") {
+//  alert("You didn't answer question 3");
+//  //return false;
+// }
+// if (q4 === null || q4 === "") {
+//  alert("You didn't answer question 4");
+//  //return false;
+// }
+// if (q5 === null || q5 === "") {
+//  alert("You didn't answer question 5");
+// // return false;
+// }
+
+//checking the users answers to the correct answer array. if the score matches then the score varialbe will increase by 1. I tested these and they work. Really want to try and get the for loop to work for this. These are commented out incase the for loop doesn't work.
+// if(q1 === correctAnswers[0]) {
+// score++;
+// }
+// if(q2 === correctAnswers[1]) {
+// score++;
+// }
+// if(q3 === correctAnswers[2]) {
+// score++;
+// }
+// if(q4 === correctAnswers[3]) {
+//  score++;
+// }
+// if(q5 === correctAnswers[4]) {
+// score++;
+// }
+
+//for this for loop- I set the i variable to 1. I am trying eval again to get the correct question number. And I have correctAnswers [i-1] so that the correct answer will be pulled from the array. This is the first time I have tried i-1. I wanted to test this as I was not sure it would work but it did. 
+// for (i = 1; i <= totalQuestions; i++) {
+//  if (eval("q" + i) === correctAnswers[i - 1]) {
+//   score++;
+//  }
+// }
+//created an alert to make sure I am calculating the score correctly. This worked so will comment it out.
+//alert("You scored " + score + " out of " + totalQuestions);
+
+//for this will display the score to the user..
+//var results = document.getElementById("results");
+//results.innerHTML = "<h3> You scored <span>" + score + " out of " + totalQuestions + "</span></h3>";
+
+// I do not want the for to truly submit so I am using the return flase below.  I read about this and used the example from https://javascript.info/forms-submit
+// return false;​
+//}
